f5d366661b39e996fe316753af296249643f59a6
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-02 20:02:15 -0300

    criação de models do reports

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Report(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local = models.ForeignKey(Institution, on_delete=models.CASCADE)
+
+    nome_laudo = models.CharField(max_length=120)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    ip_usuario = models.GenericIPAddressField()
+    termo_aceitacao = models.BooleanField(default=False)
+    
+    # Campo que receberá a resposta da IA
+    resultado_ia = models.JSONField(null=True, blank=True)
