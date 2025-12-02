2addbf5b47015f5575bd4aac293bcea458199af4
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-02 20:11:57 -0300

    atualizações em models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class Roles(models.TextChoices):
+    ADMIN = "ADMIN", "Administrador"
+    TECNICO = "TECNICO", "Técnico"
+    AUDITOR = "AUDITOR", "Auditor"
+    MEDICO = "MEDICO", "Médico"
+    BIOMEDICO = "BIOMEDICO", "Biomédico"
+
+class User(AbstractUser):
+    cargo = models.CharField(max_length=50, blank=True)
+    registro_profissional = models.CharField(max_length=50, blank=True, null=True)
+
+    
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.TECNICO
+    )
