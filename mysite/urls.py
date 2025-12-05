5fb72194d229ba4759a20caae2ac0d2abbcd6ceb
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-04 22:18:38 -0300

    apps e urls add em mysite

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("institution/", include("institution.urls")),
+    path("patients/", include("patients.urls")),
+    path("users/", include("users.urls")),
+    path("reports/", include("reports.urls")),
     path("admin/", admin.site.urls),
 ]
