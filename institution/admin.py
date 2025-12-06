6c41c642dd82f4caf134da9586c859f9e95b128e
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-06 10:03:30 -0300

    migration institution itype com choices

--- institution/admin.py
+++ institution/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from institution import models
 # Register your models here.
+
+@admin.register(models.Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+  ...
--- institution/models.py
+++ institution/models.py
@@ -8,11 +8,19 @@ from django.db import models
  #cidade = models.CharField(max_length=60)
  #uf = models.CharField(max_length=2)
 
+class TypesInstitution(models.TextChoices):
+    CLINICA = 'C', ('Clinica')
+    HOSPITAL = 'H', ('Hospital')
+    UNID_MOVEL = 'UM', ('Unidade Móvel')
+
 class Institution(models.Model):
     name = models.CharField(max_length=120)
     #endereco_fisico = models.OneToOneField(Address, on_delete=models.CASCADE)
     site = models.CharField(max_length=120)
     phone = models.CharField(max_length=15)
     email = models.EmailField(max_length=120)
-    tipo = models.CharField(max_length=50)
+    itype = models.CharField(
+        max_length=2,
+        choices = TypesInstitution.choices,
+        default = TypesInstitution.HOSPITAL)
     #logo = models.ImageField(upload_to='logos/', null=True, blank=True)
