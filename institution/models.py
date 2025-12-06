b9d315f41dcde330c2dc74095e6b65c60b645305
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-06 09:44:57 -0300

    migration institution

--- institution/models.py
+++ institution/models.py
@@ -1,16 +1,18 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class Address(models.Model):
-#     rua = models.CharField(max_length=120)
-#     cep = models.CharField(max_length=20)
-#     bairro = models.CharField(max_length=60)
-#     cidade = models.CharField(max_length=60)
-#     uf = models.CharField(max_length=2)
+# # # =Create your models here.
+#class Address(models.Model):
+ #rua = models.CharField(max_length=120)
+ #cep = models.CharField(max_length=20)
+ #bairro = models.CharField(max_length=60)
+ #cidade = models.CharField(max_length=60)
+ #uf = models.CharField(max_length=2)
 
-# class Institution(models.Model):
-#     nome = models.CharField(max_length=120)
-#     endereco_fisico = models.OneToOneField(Address, on_delete=models.CASCADE)
-#     endereco_eletronico = models.EmailField()
-#     tipo = models.CharField(max_length=50)
-#     logo = models.ImageField(upload_to='logos/', null=True, blank=True)
+class Institution(models.Model):
+    name = models.CharField(max_length=120)
+    #endereco_fisico = models.OneToOneField(Address, on_delete=models.CASCADE)
+    site = models.CharField(max_length=120)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=120)
+    tipo = models.CharField(max_length=50)
+    #logo = models.ImageField(upload_to='logos/', null=True, blank=True)
