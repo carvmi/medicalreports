6e98f33aa1feb55810b582e13b5e19828b49356f
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-13 09:42:25 -0300

    adição template medprofiles

--- medprofiles/admin.py
+++ medprofiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from medprofiles import models
 # Register your models here.
+
+@admin.register(models.Medprofile)
+class MedprofileAdmin(admin.ModelAdmin):
+  ...
--- medprofiles/models.py
+++ medprofiles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Institution(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class HealthProfessional(models.Model):
+    full_name = models.CharField(max_length=150)
+    position = models.CharField(max_length=100)
+    institution = models.ForeignKey(
+        Institution,
+        on_delete=models.CASCADE,
+        related_name="professionals"
+    )
+    specialization = models.CharField(max_length=120)
+    professional_registration = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.full_name
