15ef99b3f96a1d5ad6b8a5af0f1bd0f8c7c9f2d9
Milena <105459743+carvmi@users.noreply.github.com>
2025-12-26 08:38:51 -0300

    feat: add views plist

--- patients/views.py
+++ patients/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from patients.models import Patient
 from django.http import HttpResponse
 from .forms import PatientForm
 
 # Create your views here.
+
+def plist(request):
+    dados = Patient.objects.all()
+    return render(
+        request,
+        'plist.html',
+        {
+            'dados':dados
+        }
+    ) 
 
 def pform(request):
  form = PatientForm()
@@ -11,6 +21,6 @@ def pform(request):
   form = PatientForm(request.POST)
   if form.is_valid():
    form.save()
-   return HttpResponse ("Sucess")
+   return redirect('plist')
  return render(request, 'pform.html', {'form': form})
  
