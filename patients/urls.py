f220f2ded5526b606b6f6d8d96ff9372b55d1dbf
Milena <105459743+carvmi@users.noreply.github.com>
2026-01-06 19:15:58 -0300

    feat: add delete button

--- patients/urls.py
+++ patients/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from patients.views import view, create, edit
+from patients.views import view, create, edit, delete
 
 urlpatterns = [
     path("add", create, name='create'),
     path("", view, name='view'),
     path("edit/<int:id>", edit, name='edit'),
+    path("delete/<int:id>", delete, name='delete'),
 ]
