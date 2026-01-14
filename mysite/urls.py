19dbe4ae40f7c8a0cc596c5c58fa21b914590bc8
Milena <105459743+carvmi@users.noreply.github.com>
2026-01-14 14:40:20 -0300

    feat: starapp login

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path("institution/", include("institution.urls")),
     path("patients/", include("patients.urls")),
