d82999735954db3fc936264244158f3cbfefc6fc
Milena <105459743+carvmi@users.noreply.github.com>
2026-01-18 19:40:43 -0300

    fix: login path

--- login/urls.py
+++ login/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from login.views import cadastro, home, login
 
 urlpatterns = [
-    path("cadastro", cadastro, name="cadastro"),
-    path("login", login, name="login"),
+    path("cadastro/", cadastro, name="cadastro"),
+    path("login/", login, name="login"),
     path("", home, name="home"),    
     
 ]
