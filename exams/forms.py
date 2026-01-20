619cf649c7b29bea05ae10c149246704fc2ff232
Milena <105459743+carvmi@users.noreply.github.com>
2026-01-20 19:58:55 -0300

    feat: add forms exams

--- exams/forms.py
+++ exams/forms.py
@@ -1,2 +1,12 @@
 from django import forms
 from .models import MammogramExam, MammogramImage
+
+class MammogramExamForm(forms.ModelForm):
+    class Meta:
+        model = MammogramExam
+        fields = '__all__'
+
+class MammogramImageForm(forms.ModelForm):
+    class Meta:
+        model = MammogramImage
+        fields = '__all__'
--- exams/models.py
+++ exams/models.py
@@ -2,8 +2,21 @@ from django.db import models
 from patients.models import Patient
 from institution.models import Institution
     
+class Status(models.TextChoices):
+        PENDING = "PENDING", "Pendente"
+        READY = "READY", "Laudo Pronto" 
+        DELIVERED = "DELIVERED", "Laudo Entregue"
+        CANCELED = "CANCELED", "Laudo Cancelado"
+
+class Result(models.TextChoices):
+        SAUDAVEL = "SAUDAVEL", "Saudavel"
+        BENIGN = "BENIGN", "Benigno"
+        MALIGNANT = "MALIGNANT", "Maligno"
+        INDETERMINATE = "INDETERMINATE", "Indeterminado"
+
 
 class MammogramExam(models.Model):
+
     patient = models.ForeignKey(
         Patient,
         on_delete=models.CASCADE,
@@ -15,15 +28,19 @@ class MammogramExam(models.Model):
     description = models.TextField(null=True, blank=True)
     result = models.CharField(
         max_length=50,
-        null=True,
-        blank=True
-    )
+        choices = Result.choices,
+        default = Result.SAUDAVEL)
+
+    itype = models.CharField(
+        max_length=9,
+        choices = Status.choices,
+        default = Status.PENDING)
     acceptance_term = models.BooleanField(default=False)
     user_ip = models.GenericIPAddressField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Mammogram - {self.patient.full_name} ({self.exam_date})"
+        return f"Exame - {self.patient.full_name} {self.exam_date} {self.itype}"
 
 class MammogramImage(models.Model):
     exam = models.ForeignKey(
